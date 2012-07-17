3cf311cdfd66772234b2b6f11506518bc42182c5
Jacob Rosenthal <jakerosenthal@gmail.com>
2012-07-16 19:00:28 -0700

    hardcoded css in js to fix js problems and reenabled js

--- javascripts/app.js
+++ javascripts/app.js
@@ -19,7 +19,7 @@ jQuery(document).ready(function ($) {
       var newImage = function() {
         var num = Math.floor(Math.random()*11) - 1;
 
-        $('.main-image-div').css("background", "url("+flickrImages[num].image_b+")");
+        $('.main-image-div').css("background", "url("+flickrImages[num].image_b+") no-repeat 50%");
         $('.float').html("<h4>"+flickrImages[num].title+"</h4>");
 
         $('.main-image-div').click( function() {
