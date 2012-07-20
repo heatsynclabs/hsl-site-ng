76331e5ba52087cb2df0103f4a60bdd6c0b4741b
Erik Wilson <erikew@gmail.com>
2012-07-19 17:08:38 -0700

    more feed

--- javascripts/feeds.js
+++ javascripts/feeds.js
@@ -6,6 +6,8 @@ google.load("feeds", "1");
 
 function ginitialize() {
     var feed = new google.feeds.Feed("http://blag.meznak.net/atom.xml");
+    feed.setNumEntries(1);
+
     feed.load(function(result) {
 	if (!result.error) {
 	    var container = document.getElementById("feed");
@@ -15,21 +17,21 @@ function ginitialize() {
 
 		var entry = result.feed.entries[i];
 		var div = document.createElement("div");
-		div.innerHTML = "<h4>"+entry.title+"</h4>"+entry.content;
-
-		var rei = /<img[^>]*src=[\"\']([^\"\'\s]*)[^>]*>/gi;
-		var rai;
-
+		var rei = /<\s*img[^>]*\s+src=[\"\']?([^\"\'\s]*)[^>]*>/gi;
+		var rai, imgs = [];
+		
 		while (rai = rei.exec(entry.content)) {
-		    if (/^\//.exec(rai[1])) {
-			rai[1] = rooturl+rai[1];
-		    }
-		    console.log(rai);
+		    if (/^\//.exec(rai[1])) rai[1] = rooturl+rai[1];
+		    imgs.push(rai[1]);
 		}
 
-		//var intro = /^(\w
-
-		div.appendChild(document.createTextNode(entry.content));
+		var intro = (/^.{0,137}/g.exec(entry.content.replace(/<[^>]*>/g,"")))[0].replace(/\S*$/g,"");
+		div.innerHTML = "<img src='"+imgs[0]+"'/>"+
+		    "<h3>"+entry.title+"</h3>"+
+		    (result.feed.author?"<h4> by "+result.feed.author+"</h4>":"")+
+		    "<p>"+intro+"... (<a href='"+entry.link+"'>more</a>)</p>";
+		
+		//div.appendChild(document.createTextNode(entry.content));
 		container.appendChild(div);
 	    }
 	}
